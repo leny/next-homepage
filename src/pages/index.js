aaba87707afe6828fe43584d9a30dff024210d72
leny <info@flatland.be>
2021-02-20 15:54:58 +0100

    Prepare homepage

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+/* leny/next-homepage
+ *
+ * /src/pages/index.js - Entry point page container
+ *
+ * coded by leny@flatLand!
+ * started at 20/02/2021
+ */
+
+const Homepage = ()=>(
+    <div>{"Hello world!"}</div>
+);
+
+export default Homepage;
