bde80f18a378039a109779c9d0e904e821803302
leny <info@flatland.be>
2021-02-20 16:15:42 +0100

    Setup Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,20 @@
  * started at 20/02/2021
  */
 
+import {Helmet} from "react-helmet";
+
 import data from "../../data/data.json";
 
 export const getStaticProps = async () => ({props:data});
 
-export default ({title}) => <div>{title}</div>;
+export default ({title}) => (
+    <div>
+        <Helmet htmlAttributes={{lang:"en"}}>
+            <title>{"leny.me - Pierre-Antoine Delnatte"}</title>
+            <meta httpEquiv={"X-Clacks-Overhead"} content={"GNU Terry Pratchett"} />
+        </Helmet>
+        <main>
+            <h1>{title}</h1>
+        </main>
+    </div>
+)
